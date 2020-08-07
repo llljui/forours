7200e5085cc90449a25f25163cd6ba2b31fed09c
lixijiang <872013164@qq.com>
2020-08-07 14:00:43 +0800

    all

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,159 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+	box-sizing: border-box;
+	padding: 0;
+	margin: 0;
+	text-decoration: none;
+	list-style-type: none;
+}
+::-webkit-scrollbar {
+  /*滚动条整体样式*/
+  width : 0px;  /*高宽分别对应横竖滚动条的尺寸*/
+  height: 0px;
+}
+::-webkit-scrollbar-thumb {
+  /*滚动条里面小方块*/
+  border-radius: 0px;
+  box-shadow   : inset 0 0 5px rgba(0, 0, 0, 0.2);
+  background   : white;
+}
+::-webkit-scrollbar-track {
+  /*滚动条里面轨道*/
+  box-shadow   : inset 0 0 0px rgba(0, 0, 0, 0.2);
+  border-radius: 0px;
+  background   : #ededed;
+}
+.ant-form-explain, .ant-form-extra{
+  text-align:left!important;
+}
+.txt-right{
+	text-align: right;
+}
+.mr-b{
+	margin-bottom: 10px;
+}
+.mr-t{
+	margin-top: 10px;
+}
+.mr-l{
+	margin-left: 10px;
+}
+.mr-r{
+	margin-right: 10px;
+}
+.ant-layout-sider-children{
+	overflow-y: auto;
+}
+.flex {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: flex;
 }
 
-#nav {
-  padding: 30px;
+.flex-row {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-box-orient: horizontal;
+    flex-direction: row;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.flex-col {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-box-orient: vertical;
+    flex-direction: column;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.flex-grow-0 {
+    min-width: 0;
+    -webkit-box-flex: 0;
+    -ms-flex-positive: 0;
+    flex-grow: 0;
+    -ms-flex-negative: 0;
+    flex-shrink: 0;
+   
+}
+
+.flex-grow-1 {
+    min-width: 0;
+    -webkit-box-flex: 1;
+    -ms-flex-positive: 1;
+    flex-grow: 1;
+    -ms-flex-negative: 1;
+    flex-shrink: 1;
+}
+
+.flex-x-center {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+}
+.flex-x-spacebetween {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: flex;
+    justify-content: space-between;
+}
+.flex-x-spacearound{
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: flex;
+    justify-content: space-around;
+}
+.flex-x-start{
+	display: -webkit-box;
+	display: -webkit-flex;
+	display: flex;
+	justify-content: flex-start;
+}
+.flex-x-end{
+	display: -webkit-box;
+	display: -webkit-flex;
+	display: flex;
+	justify-content: flex-end;
+}
+.flex-y-center {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    -ms-grid-row-align: center;
+    align-items: center;
+}
+
+.flex-y-bottom {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-box-align: end;
+    -ms-flex-align: end;
+    -ms-grid-row-align: flex-end;
+    align-items: flex-end;
+}
+.bg-img{
+	background-position: 50% 50%;
+	background-repeat: no-repeat;
+	background-size: 100%;
+	background-color: #D8D8D8;
+}
+.txt-center{
+	text-align: center;
+}
+.el-table .cell.el-tooltip>div{
+	overflow: hidden;
+	text-overflow: ellipsis;
+	white-space: nowrap;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,140 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="header" src="http://qiniu.dotregion.com/52.jpg" alt="">
+	<div class="huaban" :style="{heigt:autoheight }">
+		<img :src="huabansrc" alt="loading" :style="{width:item.w+'px',top:item.top+'px',left:item.l+'px'}" v-for="(item,index) in hlist" :key="index">
+	</div>
+	<div class="content">
+		<div class="flex-row flex-y-center flex-x-center">
+			<div class="flex-grow-0 abba">
+				此处为时间线
+				<ourtime></ourtime>
+			</div>
+		</div>
+		<div class="flex-row flex-y-center trval flex-x-center">
+			<div class="flex-grow-1">吃</div>
+			<div class="flex-grow-1">喝</div>
+			<div class="flex-grow-1">玩</div>
+			<div class="flex-grow-1">乐</div>
+		</div>
+	</div>
+	<footer class="txt-center">
+		<a class="beian" href="http://beian.miit.gov.cn/">ICP证：{{beian}}</a>
+	</footer>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import ourtime from '@/components/ourtime.vue';
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    ourtime
+  },
+  data(){
+	  return{
+		  beian:'浙ICP备17012690号-2',
+		  autoheight:'',
+		  huabansrc:'http://qiniu.dotregion.com/huaban.png',
+		  hlist:[],
+		  down:null
+	  }
+  },
+  created() {
+	let self = this;
+  	window.onresize = function(val){
+		if(window.screen.availWidth <= 1200){self.autoheight = (window.screen.availWidth*.6) +'px';return}
+		self.autoheight = (window.screen.availWidth*.6) +'px';
+	}
+	function random(min, max){
+	    return min + Math.floor(Math.random() * (max - min + 1));
+	}
+	let hlist = [];
+	// 这里有三个状态  prerender，visible 和 hidden 
+	let pageVisibleStatus = document.visibilityState; 
+	
+	// 监听页面状态
+	document.addEventListener('visibilitychange', function() {
+	  // 页面状态变化为不可见时触发 
+	  if (document.visibilityState == 'hidden') {
+		   console.log('hide');
+		  window.clearInterval(self.down);
+	  } 
+	  // 页面状态变化为可见时触发 
+	  if (document.visibilityState == 'visible') {
+		  console.log('visible');
+		  self.down = setInterval(()=>{
+		  	for(let x = 0;x<30;x++){
+		  		hlist.push({w:random(0,10),l:random(0,1200),s:1,top:random(-20,-380)})
+		  	}
+		  	self.hlist = hlist;
+		  },3500)
+	    } 
+	  }
+	);
+	
   }
 }
 </script>
+<style lang="less" scoped>
+	@keyframes down{
+		0%{
+			opacity: 1;
+		}
+		10%{
+			transform: translateX(0px) rotateY(0deg) translateY(0px);
+			opacity: 1;
+		}
+		100%{
+			transform: translateX(300px) rotateY(-7200deg) translateY(600px);
+			opacity: 0;
+		}
+	}
+	.home{
+		min-width: 1200px;
+		position: relative;
+		width: 100vw;
+		.header{
+			width: 100%;
+		}
+		.huaban{
+			position: absolute;
+			width: 100%;
+			min-width: 1200px;
+			top: 0;
+			left: 0;
+			z-index: 2;
+			img{
+				animation: down 9s 1;
+				opacity: 0;
+				width: 100%;
+				position: absolute;
+			}
+		}
+		footer{
+			padding: 20px 0;
+			border-top: 1px solid #ddd;
+			.beian{
+				font-size: 14px;
+				color: #2b2b2b;
+				font-weight: bold;
+			}
+		}
+		.content{
+			.abba{
+				height: 160px;
+				padding: 20px 0;
+			}
+			.trval{
+				width: 1000px;
+				margin: 10px auto;
+				>div{
+					height: 180px;
+					border: 1px solid #ddd;
+					margin: 0 10px;
+					border-radius: 6px;
+				}
+			}
+		}
+	}
+</style>
